31bd807b1e84fc7ce7d0fdb7c729b654f1333162
n4devops <114721662+n4devops@users.noreply.github.com>
2022-10-06 11:36:20 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -3,20 +3,25 @@ resource "aws_instance" "web" {
  
   ami           = "ami-026b57f3c383c2eec"
   instance_type = "t2.micro"
-  user_data = file("userdata.sh")
+ 
   tags = {
     Name = "vm1"
     
 
   }
+ connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("prvtkey.pem")
+    host        = self.public_ip
+  }
 
-
-/*provisioner "remote-exec" {
+provisioner "remote-exec" {
     inline = [
       "sudo yum install httpd -y",
       "sudo systemctl start httpd",
       "sudo systemctl enable httpd",
       "echo '<h1>Terraform Instance Launched Successfully</h1>' | sudo tee /var/www/html/index.html"
     ]
-  }*/
+  }
 }
