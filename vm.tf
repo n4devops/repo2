5bb88be795c507d8af6cf198170917f9e56ade71
n4devops <114721662+n4devops@users.noreply.github.com>
2022-10-07 13:54:51 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -3,7 +3,14 @@ resource "aws_instance" "web" {
  
   ami           = "ami-026b57f3c383c2eec"
   instance_type = "t2.micro"
- 
+   user_data = <<-EOF
+            #!/bin/bash
+            sudo yum update -y
+            sudo yum install httpd -y
+            sudo systemctl enable httpd
+            sudo systemctl start httpd
+            echo "<h1> Welcome ! AWS Infra created using Terraform Successfully </h1><br><h2> Message from `hostname`  </h2>" > /var/www/html/index.html
+            EOF
   tags = {
     Name = "vm1"
     
@@ -11,7 +18,7 @@ resource "aws_instance" "web" {
   }
 
 
-provisioner "remote-exec" {
+/*provisioner "remote-exec" {
     inline = [
       "sudo yum install httpd -y",
       "sudo systemctl start httpd",
@@ -24,5 +31,17 @@ provisioner "remote-exec" {
     private_key = file("tfprivtky.pem")
     host        = self.public_ip
   }
+  }*/
+  resource "aws_security_group" "demo-sg" {
+  name = "sec-grp"
+  description = "Allow HTTP  traffic via Terraform"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+ 
+}
 }
