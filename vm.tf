745a64c17db14825842e98141a2a28e9f9dd4df0
n4devops <114721662+n4devops@users.noreply.github.com>
2022-10-10 14:31:48 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   
  
   ami           = "ami-026b57f3c383c2eec"
-  instance_type = "t2.micro"
+  instance_type = "t2.micro
    user_data = <<-EOF
             #!/bin/bash
             sudo yum update -y
